6d0ff635441cb08ab280a648e9e34d97ed1be6ab
pranaykargam <sunnykargam@gmail.com>
2026-03-06 10:51:56 +0530

    RebaseTokenPool

--- src/Interfaces/IRebaseToken.sol
+++ src/Interfaces/IRebaseToken.sol
@@ -11,6 +11,14 @@ interface IRebaseToken {
     function mint(address _to, uint256 _amount) external;
 
     /**
+     * @notice Mints new tokens to a specified address with a preserved interest rate (e.g. when bridging from another chain).
+     * @param _to The address to mint tokens to.
+     * @param _amount The amount of tokens to mint.
+     * @param _userInterestRate The interest rate to apply for the recipient (from source chain).
+     */
+    function mintWithInterestRate(address _to, uint256 _amount, uint256 _userInterestRate) external;
+
+    /**
      * @notice Burns tokens from a specified address.
      * @param _from The address to burn tokens from.
      * @param _amount The amount of tokens to burn.
@@ -22,6 +30,12 @@ interface IRebaseToken {
      * @param _user The address to query.
      */
     function balanceOf(address _user) external view returns (uint256);
+
+    /**
+     * @notice Returns the current interest rate for a user (used when bridging to preserve rate on destination).
+     * @param _user The address to query.
+     */
+    function getUserInterestRate(address _user) external view returns (uint256);
 }
 
 
---
+++ src/RebaseTokenPool.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {TokenPool} from "@ccip/contracts/pools/TokenPool.sol";
+import {IERC20} from "@openzeppelin/contracts@4.8.3/token/ERC20/IERC20.sol";
+import {IRebaseToken} from "./Interfaces/IRebaseToken.sol";
+import {Pool} from "@ccip/contracts/libraries/Pool.sol"; 
+
+
+
+contract RebaseTokenPool is TokenPool {
+
+    // This pool fixes that by propagating each user's personal interest rate via CCIP messages.
+    constructor(
+        IERC20 _token,
+        address[] memory _allowlist,
+        address _rnmProxy,
+        address _router
+    ) TokenPool(_token, 18, _allowlist, _rnmProxy, _router) {
+     
+    }
+
+    function lockOrBurn(
+        Pool.LockOrBurnInV1 calldata lockOrBurnIn
+    ) public override returns (Pool.LockOrBurnOutV1 memory) {
+        _validateLockOrBurn(lockOrBurnIn);
+
+        address originalSender = lockOrBurnIn.originalSender;
+
+        // Fetch the user's current interest rate from the rebase token
+        uint256 userInterestRate = IRebaseToken(address(i_token)).getUserInterestRate(originalSender);
+
+        // Burn the specified amount of tokens from this pool contract
+        // CCIP transfers tokens to the pool before lockOrBurn is called
+        IRebaseToken(address(i_token)).burn(address(this), lockOrBurnIn.amount);
+
+        emit LockedOrBurned({
+            remoteChainSelector: lockOrBurnIn.remoteChainSelector,
+            token: address(i_token),
+            sender: msg.sender,
+            amount: lockOrBurnIn.amount
+        });
+
+        return Pool.LockOrBurnOutV1({
+            destTokenAddress: getRemoteToken(lockOrBurnIn.remoteChainSelector),
+            destPoolData: abi.encode(userInterestRate)
+        });
+    }
+
+        function releaseOrMint(
+        Pool.ReleaseOrMintInV1 calldata releaseOrMintIn
+    ) public override returns (Pool.ReleaseOrMintOutV1 memory /* releaseOrMintOut */) { // Named return optional
+        _validateReleaseOrMint(releaseOrMintIn, releaseOrMintIn.sourceDenominatedAmount);
+
+        // Decode the user interest rate sent from the source pool
+        uint256 userInterestRate = abi.decode(releaseOrMintIn.sourcePoolData, (uint256));
+        
+        // The receiver address is directly available
+        address receiver = releaseOrMintIn.receiver;
+
+        // Mint tokens to the receiver, applying the propagated interest rate
+        IRebaseToken(address(i_token)).mintWithInterestRate(
+            receiver,
+            releaseOrMintIn.sourceDenominatedAmount,
+            userInterestRate
+        );
+
+        return Pool.ReleaseOrMintOutV1({
+            destinationAmount: releaseOrMintIn.sourceDenominatedAmount
+        });
+    }
+}
+
+
